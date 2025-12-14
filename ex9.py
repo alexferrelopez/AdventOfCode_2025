dc4907182468eb16683cbe910216c4478760e0b7
Àlex Ferré López <alex.fl@students.salle.url.edu>
2025-12-14 22:39:47 +0100

    Day 9 done

--- ex9.py
+++ ex9.py
@@ -49,39 +49,24 @@ class Rectangle:
         return (abs(self.lower_left_point.x - self.upper_right_point.x) + 1) * (
                 abs(self.lower_left_point.y - self.upper_right_point.y) + 1)
 
-    def is_inside_polygon(self, vertical_lines, edges, horizontal_start_points):
+    def is_inside_polygon(self, valid_points):
         vertices = [self.lower_left_point, self.lower_right_point, self.upper_left_point, self.upper_right_point]
 
         for vertex in vertices:
-            if f"{vertex.x},{vertex.y}" in valid_points:
-                continue
-            if point_on_any_line(vertex, edges) or point_is_inside_polygon(vertex, vertical_lines,
-                                                                           horizontal_start_points):
+            if point_is_in_valid_range(vertex, valid_points):
                 continue
             else:
                 return False
 
-        # Rectangle seems valid, check all the points along the edges
-
-        for x in range(self.lower_left_point.x + 1, self.lower_right_point.x):
-            for y in [self.lower_left_point.y, self.upper_left_point.y]:
-                if f"{x},{y}" in valid_points:
-                    continue
-                vertex = Point(x, y)
-                if point_on_any_line(vertex, edges) or point_is_inside_polygon(vertex, vertical_lines,
-                                                                               horizontal_start_points):
-                    continue
-                else:
-                    return False
+        for y in [self.lower_left_point.y, self.upper_left_point.y]:
+            if range_is_in_valid_range([self.lower_left_point.x + 1, self.lower_right_point.x], valid_points, y):
+                continue
+            else:
+                return False
 
         for y in range(self.lower_left_point.y + 1, self.upper_left_point.y):
             for x in [self.lower_left_point.x, self.lower_right_point.x]:
-                if f"{x},{y}" in valid_points:
-                    continue
-                vertex = Point(x, y)
-                if point_on_any_line(vertex, edges) or point_is_inside_polygon(vertex, vertical_lines,
-                                                                               horizontal_start_points):
-                    valid_points[f"{x},{y}"] = True
+                if point_is_in_valid_range(Point(x, y), valid_points):
                     continue
                 else:
                     return False
@@ -103,6 +88,22 @@ def get_edges(vertices):
         edges.append(Line(vertices[i - 1], vertices[i]))
 
     return edges
+
+
+def point_is_in_valid_range(p, valid_points):
+    x_ranges = valid_points[p.y]
+    for i in range(0, len(x_ranges), 2):
+        if x_ranges[i] <= p.x <= x_ranges[i + 1]:
+            return True
+    return False
+
+
+def range_is_in_valid_range(x_range, valid_points, y):
+    x_ranges = valid_points[y]
+    for i in range(0, len(x_ranges), 2):
+        if x_range[0] >= x_ranges[i] and x_range[1] <= x_ranges[i + 1]:
+            return True
+    return False
 
 
 def point_is_inside_polygon(p, vertical_lines, horizontal_start_points):
@@ -132,44 +133,7 @@ def point_is_inside_polygon(p, vertical_lines, horizontal_start_points):
     return intersect_count % 2 == 1
 
 
-def run_ex(file):
-    lines = [line.split(",") for line in read_file(file)]
-
-    vertices = [Point(int(line[0]), int(line[1])) for line in lines]
-
-    edges = get_edges(vertices)
-
-    vertical_lines = [e for e in edges if e.orientation == "vertical"]
-    horizontal_lines = [e for e in edges if e.orientation == "horizontal"]
-
-    horizontal_start_points = {f"{h.start.x},{h.start.y}" for h in horizontal_lines}
-
-    sorted_edges = sorted(edges, key=lambda e: e.orientation, reverse=True)
-    sorted_edges.sort(key=lambda e: e.start.x)
-
-    miny = min([v.y for v in vertices])
-    maxy = max([v.y for v in vertices])
-    minx = min([v.x for v in vertices])
-    maxx = max([v.x for v in vertices])
-
-    '''  
-    for y in range(miny, maxy + 1):
-        for x in range(minx, maxx + 1):
-            vertex = Point(x, y)
-            if point_on_any_line(vertex, edges) or point_is_inside_polygon(vertex, vertical_lines,
-                                                                                    horizontal_start_points):
-                print("#", end="")
-            else:
-                print(".", end="")
-
-        print()
-
-    
-    print((maxx - minx))
-    print((maxy - miny))
-    print((maxx - minx)*(maxy - miny))
-    '''
-
+def calculate_valid_point_ranges(maxy, miny, sorted_edges: list):
     valid_points = {i: [] for i in range(miny, maxy + 1)}
 
     for i in range(miny, maxy + 1):
@@ -197,21 +161,56 @@ def run_ex(file):
                                 valid_points[i].append(edge.start.x)
 
                             possible_wall = False
-
-
                     else:
                         valid_points[i].append(edge.start.x)
 
+    return valid_points
+
+
+def run_ex(file):
+    lines = [line.split(",") for line in read_file(file)]
+
+    vertices = [Point(int(line[0]), int(line[1])) for line in lines]
+
+    edges = get_edges(vertices)
+
+    # vertical_lines = [e for e in edges if e.orientation == "vertical"]
+    # horizontal_lines = [e for e in edges if e.orientation == "horizontal"]
+
+    # horizontal_start_points = {f"{h.start.x},{h.start.y}" for h in horizontal_lines}
+
+    sorted_edges = sorted(edges, key=lambda e: e.orientation, reverse=True)
+    sorted_edges.sort(key=lambda e: e.start.x)
+
+    miny = min([v.y for v in vertices])
+    maxy = max([v.y for v in vertices])
+    minx = min([v.x for v in vertices])
+    maxx = max([v.x for v in vertices])
+
+    valid_points = calculate_valid_point_ranges(maxy, miny, sorted_edges)
+
     '''
+    for y in range(miny, maxy + 1):
+        for x in range(minx, maxx + 1):
+            vertex = Point(x, y)
+            if point_is_in_valid_range(vertex, valid_points):
+                pass
+                print("#", end="")
+            else:
+                pass
+                print(".", end="")
+
+        print()
+    '''
+
+    max_area = 0
     for i in range(len(vertices)):
         for j in range(i + 1, len(vertices)):
             r = Rectangle(vertices[i], vertices[j])
-            if r.is_inside_polygon(vertical_lines, edges, horizontal_start_points) and r.area > max_area:
+            if r.area > max_area and r.is_inside_polygon(valid_points):
                 max_area = r.area
 
     print(max_area)
-    
-    '''
 
     return 0
 
